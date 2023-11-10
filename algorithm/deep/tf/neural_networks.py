0f65abb1f452e6a0e07fe5afd7b7a913cf3d4f3a
giova <gcad96@gmail.com>
2023-11-10 16:51:10 +0100

    Base features added.

--- algorithm/deep/tf/neural_networks.py
+++ algorithm/deep/tf/neural_networks.py
@@ -4,18 +4,46 @@ from tensorflow import keras
 from keras import Model
 
 
-class MyModel(Model):
-    def __init__(self):
-        super(MyModel, self).__init__()
-        self.conv1 = Conv2D(filters=32,
-                            kernel_size=3, activation='relu')
-        self.flatten = Flatten()
-        self.d1 = Dense(128, activation='relu')
-        self.d2 = Dense(10)
+class ResNet1(keras.Model):
 
-    def call(self, x):
-        x = self.conv1(x)
-        x = self.flatten(x)
-        x = self.d1(x)
-        output = self.d2(x)
-        return output
+    def __init__(self):
+        super(ResNet1, self).__init__()
+        self.conv_1 = keras.layers.Conv2D(32, 3, activation="relu")
+        self.conv_2 = keras.layers.Conv2D(64, 3, activation="relu")
+        self.maxpool = keras.layers.MaxPooling2D(3)
+        self.block_1 = ResNetBlock((64, 64))
+        self.block_2 = ResNetBlock((64, 64))
+        self.conv_3 = keras.layers.Conv2D(64, 3, activation='relu')
+        self.glopool = keras.layers.GlobalAveragePooling2D()
+        self.dense_1 = keras.layers.Dense(256, activation="relu")
+        self.do = keras.layers.Dropout(0.5)
+        self.dense_2 = keras.layers.Dense(10)
+
+    def call(self, inputs):
+        x = self.conv_1(inputs)
+        x = self.conv_2(x)
+        x = self.maxpool(x)
+        x = self.block_1(x)
+        x = self.block_2(x)
+        x = self.conv_3(x)
+        x = self.glopool(x)
+        x = self.dense_1(x)
+        x = self.do(x)
+        x = self.dense_2(x)
+        return x
+
+
+class ResNetBlock(keras.layers.Layer):
+    def __init__(self, filters):
+        super(ResNetBlock, self).__init__()
+        filters1, filters2 = filters
+        # first 3x3 conv
+        self.conv2a = keras.layers.Conv2D(filters1, 3, activation='relu', padding='same')
+        # second 3x3 conv
+        self.conv2b = keras.layers.Conv2D(filters2, 3, activation='relu', padding='same')
+
+    def call(self, inputs):
+        x = self.conv2a(inputs)
+        x = self.conv2b(x)
+        x += inputs
+        return x
