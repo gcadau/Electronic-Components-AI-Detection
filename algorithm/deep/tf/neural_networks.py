583c783a59adcab4dd2ae3313b53667b4ece8772
giova <gcad96@gmail.com>
2023-11-10 23:31:56 +0100

    Base features added.

--- algorithm/deep/tf/neural_networks.py
+++ algorithm/deep/tf/neural_networks.py
@@ -1,36 +1,4 @@
-import tensorflow
-from keras.src.layers import Conv2D, Flatten, Dense
 from tensorflow import keras
-from keras import Model
-
-
-class ResNet1(keras.Model):
-
-    def __init__(self):
-        super(ResNet1, self).__init__()
-        self.conv_1 = keras.layers.Conv2D(32, 3, activation="relu")
-        self.conv_2 = keras.layers.Conv2D(64, 3, activation="relu")
-        self.maxpool = keras.layers.MaxPooling2D(3)
-        self.block_1 = ResNetBlock((64, 64))
-        self.block_2 = ResNetBlock((64, 64))
-        self.conv_3 = keras.layers.Conv2D(64, 3, activation='relu')
-        self.glopool = keras.layers.GlobalAveragePooling2D()
-        self.dense_1 = keras.layers.Dense(256, activation="relu")
-        self.do = keras.layers.Dropout(0.5)
-        self.dense_2 = keras.layers.Dense(10)
-
-    def call(self, inputs):
-        x = self.conv_1(inputs)
-        x = self.conv_2(x)
-        x = self.maxpool(x)
-        x = self.block_1(x)
-        x = self.block_2(x)
-        x = self.conv_3(x)
-        x = self.glopool(x)
-        x = self.dense_1(x)
-        x = self.do(x)
-        x = self.dense_2(x)
-        return x
 
 
 class ResNetBlock(keras.layers.Layer):
@@ -46,4 +14,32 @@ class ResNetBlock(keras.layers.Layer):
         x = self.conv2a(inputs)
         x = self.conv2b(x)
         x += inputs
+        return x
+
+
+class ResNet1(keras.Model):
+    def __init__(self, n_classes, input_shape=(128, 128, 3)):
+        super(ResNet1, self).__init__()
+        self.conv_1 = keras.layers.Conv2D(32, 3, activation="relu", input_shape=input_shape)
+        self.conv_2 = keras.layers.Conv2D(64, 3, activation="relu")
+        self.maxpool = keras.layers.MaxPooling2D(3)
+        self.block_1 = ResNetBlock((64, 64))
+        self.block_2 = ResNetBlock((64, 64))
+        self.conv_3 = keras.layers.Conv2D(64, 3, activation='relu')
+        self.glopool = keras.layers.GlobalAveragePooling2D()
+        self.dense_1 = keras.layers.Dense(256, activation="relu")
+        self.do = keras.layers.Dropout(0.5)
+        self.dense_2 = keras.layers.Dense(n_classes)
+
+    def call(self, inputs):
+        x = self.conv_1(inputs)
+        x = self.conv_2(x)
+        x = self.maxpool(x)
+        x = self.block_1(x)
+        x = self.block_2(x)
+        x = self.conv_3(x)
+        x = self.glopool(x)
+        x = self.dense_1(x)
+        x = self.do(x)
+        x = self.dense_2(x)
         return x
