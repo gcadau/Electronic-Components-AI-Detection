cc220fdea90d077db62e741357c43ca1d0594b67
giova <gcad96@gmail.com>
2023-11-28 17:30:07 +0100

    Refactoring for domain randomization support (while training). Base methods.

---
+++ algorithm/domain_randomization/tf/base/layers.py
@@ -0,0 +1,20 @@
+from tensorflow import keras
+import tensorflow as tf
+
+
+class RandomInvert(keras.layers.Layer):
+
+    def __init__(self, factor=0.5, **kwargs):
+        super().__init__(**kwargs)
+        self.factor = factor
+
+    def call(self, x):
+        return self.__random_invert_img(x)
+
+
+    def __random_invert_img(x, p=0.5):
+        if tf.random.uniform([]) < p:
+            x_tf = (255 - x)
+        else:
+            x_tf = x
+        return x_tf
