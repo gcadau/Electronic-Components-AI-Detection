0a07de765c1e3ee8ed3946aa443bc52db1e498cd
Giovanni Cadau <ing.giovannicadau@gmail.com>
2024-06-04 10:47:00 +0200

    Minor bug fixing.

--- object_detection.py
+++ object_detection.py
@@ -1,5 +1,6 @@
 from algorithm.utils.data.tf.fullimage import FullImage
 from algorithm.utils.data.tf.fullimage import sliding_window, preprocess_windows
+import tensorflow as tf
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import sys
