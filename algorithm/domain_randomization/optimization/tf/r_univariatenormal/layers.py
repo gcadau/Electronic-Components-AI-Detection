7b3bc8127d11907f176fcf2f1ff40a949997d74f
Giovanni Cadau <ing.giovannicadau@gmail.com>
2026-08-17 10:52:16 +0000

    project cleanup and organization

--- algorithm/domain_randomization/optimization/tf/r_univariatenormal/layers.py
+++ algorithm/domain_randomization/optimization/tf/r_univariatenormal/layers.py
@@ -61,7 +61,7 @@ class RandomBrightness(keras.layers.Layer):
                     if self.seed is not None:
                         tf.random.set_seed(self.seed)
                         rng = np.random.default_rng(seed=self.seed)
-                        delta = np.random.normal(mean, variance, size=1)[0]
+                        delta = rng.normal(mean, variance, size=1)[0]
                     else:
                         delta = np.random.normal(mean, variance, size=1)[0]
                     if random.random() <= self.factor:
@@ -123,7 +123,7 @@ class RandomContrast(keras.layers.Layer):
                     if self.seed is not None:
                         tf.random.set_seed(self.seed)
                         rng = np.random.default_rng(seed=self.seed)
-                        contrast_factor = np.random.normal(mean, variance, size=1)[0]
+                        contrast_factor = rng.normal(mean, variance, size=1)[0]
                     else:
                         contrast_factor = np.random.normal(mean, variance, size=1)[0]
                     if random.random() <= self.factor:
@@ -189,7 +189,7 @@ class RandomHorizontallyFlip(keras.layers.Layer):
                     if self.seed is not None:
                         tf.random.set_seed(self.seed)
                         rng = np.random.default_rng(seed=self.seed)
-                        prob = np.random.normal(mean, variance, size=1)[0]
+                        prob = rng.normal(mean, variance, size=1)[0]
                     else:
                         prob = np.random.normal(mean, variance, size=1)[0]
                     if random.random() <= self.factor and prob[i] < 0.5:
@@ -252,7 +252,7 @@ class RandomVerticallyFlip(keras.layers.Layer):
                     if self.seed is not None:
                         tf.random.set_seed(self.seed)
                         rng = np.random.default_rng(seed=self.seed)
-                        prob = np.random.normal(mean, variance, size=1)[0]
+                        prob = rng.normal(mean, variance, size=1)[0]
                     else:
                         prob = np.random.normal(mean, variance, size=1)[0]
                     if random.random() <= self.factor and prob[i] < 0.5:
@@ -316,7 +316,7 @@ class RandomHue(keras.layers.Layer):
                     if self.seed is not None:
                         tf.random.set_seed(self.seed)
                         rng = np.random.default_rng(seed=self.seed)
-                        delta = np.random.normal(mean, variance, size=1)[0]
+                        delta = rng.normal(mean, variance, size=1)[0]
                     else:
                         delta = np.random.normal(mean, variance, size=1)[0]
                     if random.random() <= self.factor:
@@ -379,7 +379,7 @@ class RandomJpegQuality(keras.layers.Layer):
                     if self.seed is not None:
                         tf.random.set_seed(self.seed)
                         rng = np.random.default_rng(seed=self.seed)
-                        jpeg_quality = np.random.normal(mean, variance, size=1)[0]
+                        jpeg_quality = rng.normal(mean, variance, size=1)[0]
                     else:
                         jpeg_quality = np.random.normal(mean, variance, size=1)[0]
                     if random.random() <= self.factor:
@@ -442,7 +442,7 @@ class RandomSaturation(keras.layers.Layer):
                     if self.seed is not None:
                         tf.random.set_seed(self.seed)
                         rng = np.random.default_rng(seed=self.seed)
-                        saturation_factor = np.random.normal(mean, variance, size=1)[0]
+                        saturation_factor = rng.normal(mean, variance, size=1)[0]
                     else:
                         saturation_factor = np.random.normal(mean, variance, size=1)[0]
                     if random.random() <= self.factor:
