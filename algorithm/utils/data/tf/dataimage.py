470b2dadffdd4c9d1b615370985922bdb09ad149
Giovanni Cadau <ing.giovannicadau@gmail.com>
2026-08-19 11:30:33 +0000

    project cleanup and organization

--- algorithm/utils/data/tf/dataimage.py
+++ algorithm/utils/data/tf/dataimage.py
@@ -20,7 +20,7 @@ class DataImage:
         default_split, only_train_split = 0.2, 1
 
         if os.path.exists(self.data_path):
-            self.data = tf.data.Dataset.list_files(f"{self.data_path}\\*", shuffle=False)
+            self.data = tf.data.Dataset.list_files(f"{self.data_path}/*", shuffle=False)
         else:
             raise NotFoundDirectoryException(self.data_path)
 
